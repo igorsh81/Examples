f228e16484063c8669ff85866a923f65566b361e
igorsh81 <107439901+igorsh81@users.noreply.github.com>
2023-02-09 00:58:56 +0300

    Random

---
+++ Example_05/Program.cs
@@ -0,0 +1,6 @@
+﻿int numbersA = new Random().Next(1, 10); // 1.2.3...10 //
+Console.WriteLine(numbersA);
+int numbersB = new Random().Next(1, 10);
+Console.WriteLine(numbersB);
+int result = numbersA + numbersB;
+Console.WriteLine(result);
